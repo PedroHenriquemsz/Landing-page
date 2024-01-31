6cb7e8194a4b9e0a1134ef9ee14da3e68cab6f55
unknown <pedro.henrisz@gmail.com>
2024-01-31 12:14:19 -0300

    Commit inicial

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { NewsFormComponent } from '../news-form/news-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsFormComponent, BenefitComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/components/news-form/news-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, signal } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewletterService } from '../../services/newletter.service';
+
+@Component({
+  selector: 'app-news-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  providers:[NewletterService],
+  templateUrl: './news-form.component.html',
+  styleUrl: './news-form.component.scss'
+})
+export class NewsFormComponent {
+  newsletterForm!: FormGroup;
+  loading = signal(false)
+
+  constructor(private  service: NewletterService) {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
+
+  OnSubmit() {
+    this.loading.set(true);
+    if(this.newsletterForm.valid){
+      this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
+        next: () => {
+          this.newsletterForm.reset();
+          this.loading.set(false);
+        }
+      })
+    }
+  }
+
+}
